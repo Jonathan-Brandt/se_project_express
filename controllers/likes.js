87c6935dffd27fa96e86967d60c09584e2c70396
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-06-25 16:50:51 -0500

    Project 12 Final (I hope lol)

--- controllers/likes.js
+++ controllers/likes.js
@@ -5,41 +5,43 @@ const { BAD_REQUEST, NOT_FOUND, DEFAULT } = require("../utils/errors");
 
 const likeItem = (req, res) =>
   ClothingItem.findByIdAndUpdate(
-    req.params.temId,
+    req.params.itemId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
-      .then((user) => res.status(201).send(user))
-      .catch((err) => {
-        console.error(err);
-        if (err.name === "DocumentNotFoundError") {
-          return res.status(NOT_FOUND).send({ message: "Resource not found" });
-        } else if (err.name === "CastError") {
-          return res.status(BAD_REQUEST).send({ message: "Invalid data" });
-        }
-        return res
-          .status(DEFAULT)
-          .send({ message: "An error has occurred on the server" });
-      })
-  );
+  )
+    .orFail()
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: "Resource not found" });
+      } else if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occurred on the server" });
+    });
 
 const dislikeItem = (req, res) =>
   ClothingItem.findByIdAndUpdate(
-    req.params.temId,
+    req.params.itemId,
     { $pull: { likes: req.user._id } },
     { new: true }
-      .then((user) => res.status(201).send(user))
-      .catch((err) => {
-        console.error(err);
-        if (err.name === "DocumentNotFoundError") {
-          return res.status(NOT_FOUND).send({ message: "Resource not found" });
-        } else if (err.name === "CastError") {
-          return res.status(BAD_REQUEST).send({ message: "Invalid data" });
-        }
-        return res
-          .status(DEFAULT)
-          .send({ message: "An error has occurred on the server" });
-      })
-  );
+  )
+    .orFail()
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: "Resource not found" });
+      } else if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occurred on the server" });
+    });
 
 module.exports = {
   likeItem,
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothesRouter = require("./clothingItems");
+const likesRouter = require("./likes");
+const { BAD_REQUEST, NOT_FOUND, DEFAULT } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothesRouter);
+router.use("./likes", likesRouter);
+
+res.status(NOT_FOUND).json({ message: "Resource not found" });
 
 module.exports = router;
