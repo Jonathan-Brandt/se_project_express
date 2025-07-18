9c674d52f93ab50c20f6910de3b4301b6faa7a70
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-07-18 15:49:56 -0500

    Project 13 more auth wip

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const likesRouter = require("./likes");
 const { login } = require("../controllers/login");
 const { createUser } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
+const { authMiddleware } = require("../middlewares/auth");
 
-router.use("/users", userRouter);
-router.use("/items", clothesRouter);
-router.use("/items", likesRouter);
+router.use("/users", userRouter, authMiddleware);
+router.use("/items", clothesRouter, authMiddleware);
+router.use("/items", likesRouter, authMiddleware);
 
 router.post("/signin", login);
 router.post("/signup", createUser);
