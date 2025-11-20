4d17757e6af0a96e5e4123148b4d4d2f56af582c
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-11-20 16:06:03 -0600

    Project 15 WIP

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,4 @@
-const JWT_SECRET = "Crustaceans-are-the-best-it-is-not-up-for-debate";
+const { JWT_SECRET = "Crustaceans-are-the-best-it-is-not-up-for-debate" } =
+  process.env;
 
 module.exports = { JWT_SECRET };
