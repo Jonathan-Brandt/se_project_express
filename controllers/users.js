a4489c4ded5374a9e5dae85ff57e871d170419b6
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-11-27 12:09:37 -0600

    Project 15 more wip

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const { JWT_SECRET } = require("../utils/config");
+const jwt = require("jsonwebtoken");
 
 const {
   BAD_REQUEST,
@@ -88,9 +90,40 @@ const updateProfile = (req, res) => {
     });
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "Please enter email and password" });
+  }
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      return res.send({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.message === "invalid emal or password") {
+        return res.status(AUTHERROR).send({
+          message:
+            "The credentials do not match those in our records, please try again",
+        });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
 module.exports = {
   getUsers,
   getCurrentUser,
   createUser,
   updateProfile,
+  login,
 };
